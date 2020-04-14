b6ee671140b07715a38e9aab3fd21a71c750fb82
Dedi Rahmansah <dedir2@gmail.com>
2020-04-14 17:28:48 +0700

    delete dosen tidak tetap

--- app/Http/Controllers/DosenTidakTetapController.php
+++ app/Http/Controllers/DosenTidakTetapController.php
@@ -126,6 +126,16 @@ class DosenTidakTetapController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $data = \App\DosenTidakTetapModel::where('id',$id)->first();
+    
+        if($data->delete()){
+            $res['message'] = "Success!";
+            $res['value'] = "$data";
+            return response($res);
+        }
+        else{
+            $res['message'] = "Failed!";
+            return response($res);
+        }
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -41,7 +41,7 @@ Route::post('/rekognisi_dosen/create','RekognisiDosenController@store');
 
 //method delete
 Route::post('/dosen_tetap/delete/{id}','DosenTetapController@destroy');
-Route::post('/dosen_tidak_tetap/create','DosenTidakTetapController@store');
+Route::post('/dosen_tidak_tetap/delete/{id}','DosenTidakTetapController@destroy');
 Route::post('/dosen_pembimbing/create','DosenPembimbingController@store');
 Route::post('/dosen_industri/create','DosenIndustriController@store');
 Route::post('/ewmp_dosen/create','EWMPDosenController@store');
