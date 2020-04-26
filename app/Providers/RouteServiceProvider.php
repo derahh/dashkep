ad5d4975d74d9556bc060446909ef981cce21d0b
Dedi Rahmansah <dedir2@gmail.com>
2020-04-26 14:11:41 +0700

    remove mapApiRoutes()

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -65,10 +65,10 @@ class RouteServiceProvider extends ServiceProvider
      */
     protected function mapApiRoutes()
     {
-        Route::prefix('api')
-             ->middleware('api')
-             ->namespace($this->namespace)
-             ->group(base_path('routes/api.php'));
+        // Route::prefix('api')
+        //      ->middleware('api')
+        //      ->namespace($this->namespace)
+        //      ->group(base_path('routes/api.php'));
 
         Route::group([
             'middleware' => ['api', 'cors'],
